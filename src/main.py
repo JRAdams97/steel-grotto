3568159ee3bb5c4a612cafd7373962baa0e41caa
JRAdams97 <justinadams7991@gmail.com>
2019-07-05 01:35:16 +1000

    feat(init): Setup game configuration
    - add basic YAML file to hold game properties
    - add property decorators to Game class in main.py
    - modify and uplift main.py to conform to PEP 8
    
    closes #STL-0002

--- src/main.py
+++ src/main.py
@@ -1,27 +1,67 @@
 import pygame
+import yaml
 from pygame.locals import *
 
 
 class Game:
 
-    WINDOW_WIDTH = 640
-    WINDOW_HEIGHT = 480
-    X = 10
-    Y = 10
-
     def __init__(self):
-        self._running = True
+        self._config = None
         self._display_surf = None
-
-    def on_init(self):
-        pygame.init()
-        self._display_surf = pygame.display.set_mode((self.WINDOW_WIDTH, self.WINDOW_HEIGHT),
-                                                     pygame.HWSURFACE)
         self._running = True
 
+    # -----------------------------------------------------------------
+    # Property Access
+    # -----------------------------------------------------------------
+    @property
+    def config(self):
+        return self._config
+
+    @config.setter
+    def config(self, n_config):
+        self._config = n_config
+
+    @property
+    def display_surf(self):
+        return self._display_surf
+
+    @display_surf.setter
+    def display_surf(self, n_display_surf):
+        self._display_surf = n_display_surf
+
+    @property
+    def running(self):
+        return self._running
+
+    @running.setter
+    def running(self, n_running):
+        self._running = n_running
+
+    # -----------------------------------------------------------------
+    # Private Class Functions
+    # -----------------------------------------------------------------
+    def _on_init(self):
+        pygame.init()
+
+        self.config = self._load_configuration()
+        self.display_surf = pygame.display.set_mode((self.config['game_window']['width'],
+                                                     self.config['game_window']['height']))
+        return self.display_surf
+
+    @staticmethod
+    def _load_configuration():
+        with open("conf/grotto.yaml", 'r') as stream:
+            try:
+                return yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+    # -----------------------------------------------------------------
+    # Public Class Functions
+    # -----------------------------------------------------------------
     def on_event(self, event):
         if event.type == QUIT:
-            self._running = False
+            self.running = False
 
     def on_loop(self):
         pass
@@ -34,14 +74,17 @@ class Game:
         pygame.quit()
 
     def on_execute(self):
-        if not self.on_init():
-            self._running = False
+        try:
+            self._on_init()
+        except pygame.error:
+            self.running = False
 
-        while self._running:
+        while self.running:
             for event in pygame.event.get():
                 self.on_event(event)
             self.on_loop()
             self.on_render()
+
         self.on_cleanup()
 
 
